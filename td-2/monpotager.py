697f150b9f82ec189e60ec9bfae14bdcf83873e3
Laurens Marill <laurens.marill@skf.com>
2022-04-04 08:18:10 -0400

    Implementation de classes

--- td-2/monpotager.py
+++ td-2/monpotager.py
@@ -1,13 +1,69 @@
 
 class Primeur():
 
-    nom = ""
+    _nom = ""
+    _portager = []
 
-    def __init__(self, nom = "Toto"):
-        self.nom = nom
-        print("bonjour je m'appelle: " + self.nom)
+    def __init__(self, nom = "Gerard"):
+        self._nom = nom
+        print("bonjour je m'appelle %s " % self._nom)
 
-# Constructeurs
-primeur1 = Primeur()
-primeur2 = Primeur("Popo")
+    def plante(self, fruitOuLegume):
+        raise NotImplementedError
 
+    def arroseZone(self, zone):
+        raise NotImplementedError
+
+    def ajouteZone(self, zone):
+        raise NotImplementedError
+
+class Zone():
+    _largeur = 0
+    _longueur = 0
+
+    @property
+    def largeur(self):
+        return self._largeur
+
+    @largeur.setter
+    def largeur(self, largeur):
+        self._largeur = largeur
+
+    @property
+    def longueur(self):
+        return self._longueur
+
+    @longueur.setter
+    def longueur(self, longueur):
+        self._longueur = longueur
+
+class ZoneLegumes(Zone):
+    _nom = "Mes Legumes"
+    _legumes = []
+
+    def __init__(self, largeur, longueur):
+        super().__init__(self, largeur, longueur)
+
+class ZoneFruits(Zone):
+    _nom = "Mes Fruits"
+    _fruits = []
+
+    def __init__(self, largeur, longueur):
+        super().__init__(self, largeur, longueur)
+
+class Fruit():
+
+    def __init__(self, nom):
+        self._nom = nom
+        print("fruit %s créé avec succès!" % self._nom)
+
+class Legume():
+
+    def __init__(self, nom):
+        self._nom = nom
+        print("légume %s créé avec succès!" % self._nom)
+
+
+### MAIN ###
+if __name__ == '__main__':
+    primeur = Primeur()
