c72a2570dac6cffa4fcba996550f99cebcf28abe
Laurens Marill <laurens.marill@skf.com>
2022-04-06 10:55:53 -0400

    ajout age

--- td-3/chat.py
+++ td-3/chat.py
@@ -13,15 +13,20 @@ class Classification(Enum):
 class Animal():
     _classification = None
     _name = ""
+    _age = None
 
-    def __init__(self, name, classification):
+    def __init__(self, name, age, classification):
         if not isinstance(name, str):
             raise ArgumentTypeError("must be a string!")
+
+        if not isinstance(age, int):
+            raise ArgumentTypeError("must be an integer!")
 
         if not isinstance(classification, Classification):
             raise ArgumentTypeError("must be a Classification!")
 
         self._name = name
+        self._age = age
         self._classification = classification
 
     @property
@@ -32,8 +37,12 @@ class Animal():
     def name(self):
         return self._name
 
+    @property
+    def age(self):
+        return self._age
+
 class Chat(Animal):
     _isCute = True
 
-    def __init__(self, name):
-        super().__init__(name, Classification.MAMIFERE)
+    def __init__(self, name, age):
+        super().__init__(name, age, Classification.MAMIFERE)
--- td-3/tests_chat.py
+++ td-3/tests_chat.py
@@ -7,19 +7,28 @@ class TesterMonCat(unittest.TestCase):
     
     def test_constructor_nominal(self):
         """Test constructeur nominal"""
-        monchat = chat.Chat("Zélda")
+        monchat = chat.Chat("Zélda", 18)
         self.assertEqual(monchat.name, "Zélda")
         self.assertEqual(monchat.classification, chat.Classification.MAMIFERE)
         self.assertIsInstance(monchat.classification, chat.Classification)
         self.assertEqual(monchat._isCute, True)
+        self.assertEqual(monchat.age, 18)
+
 
     
-    def test_constructor_argumenttypeerror(self):
+    def test_constructor_argumenttypeerror_name(self):
         """Test constructeur ArgumentTypeError"""
-        self.assertRaises(ArgumentTypeError, chat.Chat, 2)
-        self.assertRaises(ArgumentTypeError, chat.Chat, [1,"2",3,4])
-        self.assertRaises(ArgumentTypeError, chat.Chat, {"1":1 })
-        self.assertRaises(ArgumentTypeError, chat.Chat, (1,2,3))
+        self.assertRaises(ArgumentTypeError, chat.Chat, 2, 18)
+        self.assertRaises(ArgumentTypeError, chat.Chat, [1,"2",3,4], 18)
+        self.assertRaises(ArgumentTypeError, chat.Chat, {"1":1 }, 18)
+        self.assertRaises(ArgumentTypeError, chat.Chat, (1,2,3), 18)
+
+    def test_constructor_argumenttypeerror_age(self):
+        """Test constructeur ArgumentTypeError"""
+        self.assertRaises(ArgumentTypeError, chat.Chat, "Zélda", "")
+        self.assertRaises(ArgumentTypeError, chat.Chat, "Zélda", [])
+        self.assertRaises(ArgumentTypeError, chat.Chat, "Zélda", {})
+        self.assertRaises(ArgumentTypeError, chat.Chat, "Zélda", ())
 
 if __name__ == '__main__':
     unittest.main()
